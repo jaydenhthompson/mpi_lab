92b862d517a2aa37270a20407bdb311387a0295d
Jayden <jaydenhthompson@gmail.com>
2022-12-02 18:09:12 -0500

    Distribute work

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,16 @@
 #include <mpi.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main(int argc, char **argv) {
-  int id, numProcesses;
+  int id, num_processes;
 
   options_t opts;
   get_opts(argc, argv, opts);
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
   std::ifstream in(opts.in_file);
@@ -29,11 +30,27 @@ int main(int argc, char **argv) {
   window * wind = new window(0, 4, 0, 4);
   quadtree tree(bodies, wind);
 
-  if (id == 0) {
-    // parent
+  auto work_size = num_bodies < num_processes ? num_processes : num_bodies % num_processes == 0 ? num_bodies
+                                                                                                : num_bodies + (num_processes - (num_bodies % num_processes));
+  std::vector<int> work(work_size, -1);
+  for(int i = 0; i < num_bodies; i++)
+  {
+    work[i] = i;
   }
-  else {
-    // child
+
+  std::cout << "work size" << work_size << std::endl;
+
+  int per_proc = work_size / num_processes;
+  stc::vector<int> my_work(per_proc);
+  MPI_Scatter(work.data(), per_proc, MPI_INT, my_work.data(), per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+  // TODO : MPI_Gather(my_work, per_proc, MPI_INT, work.data(), per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+  if (id == 0) // parent
+  { 
+  }
+  else // child 
+  {
   }
 
   MPI_Finalize();
